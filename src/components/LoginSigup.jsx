40ec2da4219fddba018c67d24ead8edc308d7476
fardeen077 <fardeen36143@gmail.com>
2024-12-14 19:11:09 +0530

    push

--- src/components/LoginSigup.jsx
+++ src/components/LoginSigup.jsx
@@ -1,25 +1,28 @@
 import { UserIcon } from '@heroicons/react/24/solid';
 import { EnvelopeIcon } from '@heroicons/react/24/solid';
 import { LockClosedIcon } from '@heroicons/react/24/solid';
+import { useState } from 'react';
 
 function LoginSignup() {
+    const [action, setAction] = useState("Login");
+
     return (
         <div className='flex flex-col justify-center items-center m-auto mt-40 myfont rounded-lg shadow-2xl max-w-xl gap-7 text-black p-10 '>
-            <div className='p-5 text-2xl font-bold'>
-                Sign Up
-                <span className='w-10 h-2 bg-white '></span>
+            <div className='p-5 text-4xl font-bold '>
+                {action}
             </div>
 
             {/* Name Input */}
-            <div className="flex items-center justify-center m-auto w-full h-20 bg-slate-200 rounded-lg">
+            {action==="Sign Up" && (
+                 <div className="flex items-center justify-center m-auto w-full h-20 bg-slate-200 rounded-lg">
                 <UserIcon className="h-6 w-6 m-3" />
                 <input
                     type="text"
                     placeholder="Name"
                     className='h-12 w-full  bg-transparent border-none outline-none text-lg '
                 />
-            </div>
-
+            </div> 
+            )}
             {/* Email Input */}
             <div className="flex items-center justify-center m-auto w-full h-20 bg-slate-200 rounded-lg">
                 <EnvelopeIcon className="h-6 w-6 m-3" />
@@ -41,21 +44,27 @@ function LoginSignup() {
             </div>
 
             {/* Lost Password */}
-            <div className='gap-1 text-black text-sm flex'>
+            {action === "Login" &&( <div className='gap-1 text-black text-sm flex'>
                 <h1>Lost Password?</h1>
                 <span className='gap-3 text-blue-700 cursor-pointer'>Click Here!</span>
-            </div>
+            </div>)}
 
             {/* Buttons */}
-            <div className='flex gap-3'>
-                <button className='rounded-lg px-4 py-2 bg-black text-white hover:bg-gray-800'>
+            <div className='flex gap-4'>
+                <button
+                    onClick={() => setAction("Sign Up")}
+                    className={`rounded-lg px-4 py-2 ${action === "Sign Up" ? "bg-black text-white" : "bg-gray-300 text-black"
+                        } hover:bg-gray-200 hover:text-black`}>
                     Sign Up
                 </button>
-                <button className='rounded-lg px-4 py-2 bg-gray-300 text-black hover:bg-gray-400'>
+                <button
+                    onClick={() => setAction("Login")}
+                    className={`rounded-lg px-4 py-2 ${action === "Login" ? "bg-black text-white" : "bg-gray-300 text-black"
+                        } hover:bg-gray-200 hover:text-black`}>
                     Login
                 </button>
             </div>
-        </div>
+        </div >
     );
 }
 export default LoginSignup;
